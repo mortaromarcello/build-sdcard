e8d5314d8af5f85d70ac7e23dc6c3068b62dde12
mortaromarcello <mortaromarcello@gmail.com>
2013-01-30 09:10:16 -0800

    git.sh

---
+++ git.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+git clone https://github.com/mortaromarcello/buildroot-A10.git
+git clone https://github.com/linux-sunxi/sunxi-tools.git
+git clone https://github.com/hno/uboot-allwinner.git
