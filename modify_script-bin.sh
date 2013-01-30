27f6ff04179d82548fcc78d51093226fe045ede5
mortaromarcello <mortaromarcello@gmail.com>
2013-01-30 15:34:05 +0100

    Update modify_script-bin.sh

--- modify_script-bin.sh
+++ modify_script-bin.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 DIR=$(pwd)
-cd $DIR/sunxi-tools
+cd ${DIR}/sunxi-tools
 make
 sed -e 's/screen0_output_type = [0-9]/screen0_output_type = 1/' -e 's/screen0_output_mode = [0-9]/screen0_output_mode = 0/' script.fex > scriptmod.fex
 ./fex2bin script.fex scriptmod.bin
 if [ ! -d $DIR/output ]; then
-	mkdir -p $DIR/output
+	mkdir -p ${DIR}/output
 fi
-cp -vf $DIR/sunxi-tools/scriptmod.bin $DIR/output/script.bin
-cd $DIR
+cp -vf $DIR/sunxi-tools/scriptmod.bin ${DIR}/output/script.bin
+cd ${DIR}
