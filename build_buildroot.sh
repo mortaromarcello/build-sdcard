66b615b42d4780b3c2cd94be750fb3517cb40f37
mortaromarcello <mortaromarcello@gmail.com>
2013-01-30 15:32:23 +0100

    Update build_buildroot.sh

--- build_buildroot.sh
+++ build_buildroot.sh
@@ -1,7 +1,7 @@
+#!/usr/bin/env bash
 DIR=$(pwd)
 OUT=${DIR}/out_buildroot
-BUILDROOT=${DIR}/buildroot-allwinner
+BUILDROOT=${DIR}/buildroot-A10
 cd ${BUILDROOT}
 make allwinner_defconfig
 make O=${OUT} -C ${BUILDROOT} BUILDROOT_DL_DIR=${DIR}/dl
-
